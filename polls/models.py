c43fca28a4078dc33209f9b17241b165c39e9795
mdettelson <mdettelson@gmail.com>
2017-07-17 23:03:30 -0400

    Reformatted models.py to PEP-8 standards, added admin class for Question model

--- polls/models.py
+++ polls/models.py
@@ -6,9 +6,10 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 @python_2_unicode_compatible
 class Question(models.Model):
-    question_text=models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -16,6 +17,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 @python_2_unicode_compatible
 class Choice(models.Model):
