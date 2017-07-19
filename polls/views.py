c2cba58d53df1909c43db5c663e555599c737ba2
mdettelson <mdettelson@gmail.com>
2017-07-19 18:45:27 -0400

    Added index template and updated view to use it

--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,24 @@ from __future__ import unicode_literals
 
 #  from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 from polls.models import Question
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context_dict = {'latest_question_list': latest_question_list}
+    # this and the above template line can be abbreviated to:
+    # return render(request, 'polls/index.html', context_dict)
+    # using django.shortcuts.render
+    return HttpResponse(template.render(context_dict, request))
 
 
 def detail(request, question_id):
+    
     return HttpResponse("You're looking at question {}.".format(question_id))
 
 
