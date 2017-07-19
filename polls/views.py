f3aa1b8030401676d1b81acd301958f5511e637c
mdettelson <mdettelson@gmail.com>
2017-07-19 18:15:34 -0400

    Added new boilerplate endpoints to polls app

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+#  from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey there!  You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question {}.".format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
