c6764a16b574461d0e0c8a8d03607520afd3e862
mdettelson <mdettelson@gmail.com>
2017-07-20 01:23:11 -0400

    Updated views to use generics

--- polls/views.py
+++ polls/views.py
@@ -2,36 +2,32 @@
 from __future__ import unicode_literals
 
 #  from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.template import loader
 from django.urls import reverse
+from django.views import generic
 from django.db.models import F
+
 from polls.models import Question, Choice
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context_dict = {'latest_question_list': latest_question_list}
-    # this and the above template line can be abbreviated to:
-    # return render(request, 'polls/index.html', context_dict)
-    # using django.shortcuts.render
-    return HttpResponse(template.render(context_dict, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    # the following code should only run when
-    # a question with the above id exists
-    context_dict = {'question': question}
-    return render(request, 'polls/detail.html', context_dict)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
